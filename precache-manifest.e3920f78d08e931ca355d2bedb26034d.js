b4f3dd47e0006967426f9e32437cffe8531dcea0
Mahi Velaga <mahitavelaga@gmail.com>
2020-03-14 11:37:38 -0500

    first

---
+++ precache-manifest.e3920f78d08e931ca355d2bedb26034d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57a336cb058a02f9c54c3a0449407028",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4b5a7daf3530c1132f9",
+    "url": "/static/css/main.8a557483.chunk.css"
+  },
+  {
+    "revision": "89536232f44eaa24fe6b",
+    "url": "/static/js/2.7fc6b809.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.7fc6b809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4b5a7daf3530c1132f9",
+    "url": "/static/js/main.a5066bb1.chunk.js"
+  },
+  {
+    "revision": "d2e3d287abbff0085f2e",
+    "url": "/static/js/runtime-main.9d651b5c.js"
+  }
+]);
